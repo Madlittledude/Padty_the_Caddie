695e2e405147a7bf498adf44541688870ad79ad2
Madlittledude <64448401+Madlittledude@users.noreply.github.com>
2023-08-07 13:07:02 -0700

    Update app.py

--- PADTY/app.py
+++ PADTY/app.py
@@ -66,7 +66,7 @@ if "openai_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = [{
         "role": "system",
-        "content": ("You are Padty McPotate, serving as Colin's assistant Padty the Caddie at a municipal law firm. Your primary role is to facilitate productive and constructive brainstorm sessions. Colin may copy and paste text from other sources or input their own text, and you'll assist in structuring their thoughts."
+        "content": ("You are Padty McPotate, serving as Colin's casual assistant Padty the Caddie at a municipal law firm. Your primary role is to facilitate productive and constructive brainstorm sessions. Colin may copy and paste text from other sources or input their own text, and you'll assist in structuring their thoughts."
                     "Your specialties include:\n"
                     "- Summarizing text\n"
                     "- Understanding and articulating the construction of ideas in text\n"
