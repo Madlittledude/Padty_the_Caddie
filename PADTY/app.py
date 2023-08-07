3a6b0dd97971164aaba86cece6777c905fdb5b3f
Madlittledude <64448401+Madlittledude@users.noreply.github.com>
2023-08-07 12:29:32 -0700

    Update app.py

--- PADTY/app.py
+++ PADTY/app.py
@@ -41,7 +41,7 @@ def display_chat_interface():
         st.session_state.messages.append({"role": "user", "content": prompt})
         display_chat_message("user", prompt,colin)
 
-        with st.chat_message("assistant",avatar =padty):
+        with st.chat_message("assistant"):
             message_placeholder = st.empty()
             full_response = ""
             for response in openai.ChatCompletion.create(
